b20768e165f74db6d760184aa3a4610cc7b56f8e
ASHOKKUMAR SUNKARA <102138547+21a95a0428@users.noreply.github.com>
2022-11-05 05:58:39 +0530

    t=int(input()) a=list(map(int,input().split())) d=0 c=[] for i in a:     if i not in c:         c.append(i) for j in c:     if j%2==0:         d+=j print(d)

---
+++ unique_even_elements_sum.py
@@ -0,0 +1,13 @@
+t=int(input())
+a=list(map(int,input().split()))
+d=0
+c=[]
+for i in a:
+    if i not in c:
+        c.append(i)
+for j in c:
+    if j%2==0:
+        d+=j
+print(d)
+        
+    
